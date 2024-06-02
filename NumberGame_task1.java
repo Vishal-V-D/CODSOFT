c3a90a0e1609a72c9c7fc6275f9e8bcce529923d
Vishal-V-D <157345717+Vishal-V-D@users.noreply.github.com>
2024-06-02 11:42:45 +0530

    Update and rename NumberGame-task1.java to NumberGame_task1.java

--- NumberGame_task1.java
+++ NumberGame_task1.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class NumberGuessingGame {
+public class NumberGame_task1{
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
